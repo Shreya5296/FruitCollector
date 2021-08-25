8c849a50bec153dd9f504caa54985626d862a000
Shreya5296 <85358610+Shreya5296@users.noreply.github.com>
2021-08-25 22:06:18 +0530

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -29,7 +29,7 @@ function preload(){
   obstacleImage = loadImage("images/mushroom.png")
 }
 function setup() {
-  createCanvas(1000, 600);
+  createCanvas(1500, 600);
   database = firebase.database();
   game = new Game();
   game.getState();
